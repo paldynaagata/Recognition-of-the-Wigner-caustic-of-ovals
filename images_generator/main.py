3fa230ca1904ce58859711d4c0d38632254bb01b
paldynaagata <paldyna.agata@gmail.com>
2020-07-12 20:04:59 +0200

    Minor change in the way of saving generated images

--- images_generator/main.py
+++ images_generator/main.py
@@ -5,28 +5,27 @@ from wigner_caustic import WignerCaustic
 from random import randint
 
 
-def plot_curve(curve, curve_type, img_num, img_size = 64):
+def plot_curve(curve, curve_type, spikes_num, img_num, img_size = 64):
     root = "./../../images/"
 
     if curve_type == "oval":
         my_dpi = 120
         plt.figure(figsize = (img_size / my_dpi, img_size / my_dpi), dpi = my_dpi)
-        img_path = f"ovals/{img_size:d}x{img_size:d}/"
+        img_path = f"ovals/{img_size:d}x{img_size:d}/class_{spikes_num:d}/"
     elif curve_type == "wc":
         plt.figure(figsize = (8, 8))
         img_path = "wigner_caustics/"
     
     plt.plot(curve[0], curve[1], c = "black")
     plt.axis("off")
-    plt.savefig(f"{root}{img_path}{curve_type}{img_num:04d}.png")
+    plt.savefig(f"{root}{img_path}{spikes_num:d}_{curve_type}{img_num:04d}.png")
     plt.close()
 
 
 if __name__ == "__main__":
-    oval_idx = 0
-
     for spikes_num in range(3, 8, 2):
         bias = 200 * spikes_num
+        oval_idx = 0
 
         for i in range(1, 21):
             sin_params = []
@@ -43,14 +42,14 @@ if __name__ == "__main__":
 
             oval_idx += 1
 
-            print(f"### Oval no {oval_idx} ### Num of spikes {spikes_num} ### sin_params {sin_params} ### cos_params {cos_params} ###")
+            print(f"### Num of spikes {spikes_num} ### Oval no {oval_idx} ### sin_params {sin_params} ### cos_params {cos_params} ###")
 
             oval = Oval(bias, sin_params, cos_params)
             oval_parameterization = oval.parameterization()
             wc = WignerCaustic(oval)
             wc_parameterization = wc.wigner_caustic()
 
-            plot_curve(oval_parameterization, "oval", oval_idx)
-            plot_curve(oval_parameterization, "oval", oval_idx, img_size = 128)
-            plot_curve(wc_parameterization, "wc", oval_idx)
+            plot_curve(oval_parameterization, "oval", spikes_num, oval_idx)
+            plot_curve(oval_parameterization, "oval", spikes_num, oval_idx, img_size = 128)
+            plot_curve(wc_parameterization, "wc", spikes_num, oval_idx)
     
