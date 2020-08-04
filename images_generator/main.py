69e9ddce942a58ac407604315d8adfc20c43ee74
paldynaagata <paldyna.agata@gmail.com>
2020-08-04 18:29:42 +0200

    Small changes in images generator

--- images_generator/main.py
+++ images_generator/main.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+import random
+import os
 
 from oval import Oval
 from wigner_caustic import WignerCaustic
-from random import randint
 
 
 def plot_curve(curve, curve_type, spikes_num, img_num, img_size = 64):
-    root = "./../../images/"
+    root = "./../../images/v02/"
 
     if curve_type == "oval":
         my_dpi = 120
@@ -16,29 +17,48 @@ def plot_curve(curve, curve_type, spikes_num, img_num, img_size = 64):
         plt.figure(figsize = (8, 8))
         img_path = "wigner_caustics/"
     
+    directory = f"{root}{img_path}"
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
     plt.plot(curve[0], curve[1], c = "black")
     plt.axis("off")
-    plt.savefig(f"{root}{img_path}{spikes_num:d}_{curve_type}{img_num:04d}.png")
+    plt.savefig(f"{directory}{spikes_num:d}_{curve_type}{img_num:04d}.png")
     plt.close()
 
 
 if __name__ == "__main__":
     for spikes_num in range(3, 8, 2):
-        bias = 200 * spikes_num
+        # bias = 300 * spikes_num
         oval_idx = 0
+        images_per_class_num = 20
 
-        for i in range(1, 21):
+        for i in range(images_per_class_num):
             sin_params = []
             cos_params = []
 
             for j in range(spikes_num + 2):
-                rand_sin_param = randint(-3, 3)
+                if j % 2:
+                    rand_sin_param = random.uniform(10, 40)
+                    rand_cos_param = random.uniform(10, 40)
+                else:
+                    rand_sin_param = random.uniform(-3, 3)
+                    rand_cos_param = random.uniform(-3, 3)
+                    
                 sin_params.append(rand_sin_param)
-                rand_cos_param = randint(-3, 3)
                 cos_params.append(rand_cos_param)
 
-            sin_params[spikes_num - 1] = randint(10, 15)
-            cos_params[spikes_num - 1] = randint(10, 15)
+            sin_params[spikes_num - 1] = random.uniform(40, 60)
+            cos_params[spikes_num - 1] = random.uniform(40, 60)
+
+            limit = 0
+
+            for idx in range(len(sin_params)):
+                limit += ((idx + 1) ** 2 - 1) * (sin_params[idx] + cos_params[idx])
+            
+            bias = limit + 100
+            print(f"### bias: {bias} ### limit: {limit} ###")
 
             oval_idx += 1
 
