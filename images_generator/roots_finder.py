287d4558411c51ec3e44d6dbf115e7f52df9bb3c
paldynaagata <paldyna.agata@gmail.com>
2020-09-17 22:41:28 +0200

    Added limit of iterations in newton method

--- images_generator/roots_finder.py
+++ images_generator/roots_finder.py
@@ -4,17 +4,24 @@ from scipy import misc
 class RootsFinder:
     def __init__(self, f):
         self.f = f
+        self.recursions_counter = 0
 
 
     def _newton_method(self, x_i):
         x_j = x_i - self.f(x_i) / misc.derivative(self.f, x_i, dx = 1e-6, n = 1)
+
+        if self.recursions_counter > 100:
+            return None
+
         if abs(x_i - x_j) < 0.00001 or abs(self.f(x_j)) < 0.00001:
             return x_j
         else:
+            self.recursions_counter += 1
             return self._newton_method(x_j)
 
 
     def newton_method(self, x_0):
+        self.recursions_counter = 0
         return self._newton_method(x_0)
 
 
@@ -22,20 +29,20 @@ class RootsFinder:
         roots = []
         step = (b - a) / n
         x_0 = a
-        # root = 0
 
         while x_0 < b:
             new_root = self.newton_method(x_0)
-            new_root_is_new = True
 
-            for r in roots:
-                if abs(r - new_root) < 0.01:
-                    new_root_is_new = False
-                    break
+            if new_root is not None:
+                new_root_is_new = True
 
-            if (a <= new_root <= b) and (x_0 == a or new_root_is_new):
-                roots.append(new_root)
-                # root = new_root
+                for root in roots:
+                    if abs(root - new_root) < 0.01:
+                        new_root_is_new = False
+                        break
+
+                if a <= new_root < b and new_root_is_new:
+                    roots.append(new_root)
 
             x_0 += step
         
