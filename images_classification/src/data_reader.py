d863054f9663ce3dec8d412151aaf3ee6294bcbb
paldynaagata <paldyna.agata@gmail.com>
2020-10-22 15:17:19 +0200

    Changes in DataReader class method and constructor

--- images_classification/src/data_reader.py
+++ images_classification/src/data_reader.py
@@ -2,25 +2,30 @@ from tensorflow import keras
 
 
 class DataReader:
-    def __init__(self, images_dir, class_names, validation_split, subset_name, seed, image_size, batch_size):
+    def __init__(self, 
+                images_dir, 
+                image_size, 
+                batch_size, 
+                validation_split = 0.2, 
+                seed = 123456,
+                class_names = ["class_3", "class_5", "class_7"]):
         self.images_dir = images_dir
-        self.class_names = class_names
-        self.validation_split = validation_split
-        self.subset_name = subset_name
-        self.seed = seed
         self.image_size = image_size
         self.batch_size = batch_size
+        self.validation_split = validation_split
+        self.seed = seed
+        self.class_names = class_names
     
 
-    def read_data(self):
+    def read_data(self, subset_name = None):
         dataset = keras.preprocessing.image_dataset_from_directory(
             self.images_dir,
-            class_names = self.class_names,
-            validation_split = self.validation_split,
-            subset = self.subset_name,
-            seed = self.seed,
             image_size = self.image_size,
-            batch_size = self.batch_size
+            batch_size = self.batch_size,
+            validation_split = self.validation_split,
+            seed = self.seed,
+            class_names = self.class_names,
+            subset = subset_name
         )
 
         return dataset
